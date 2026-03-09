5a4b006da1265974311f413124686876de83c0d2
Dhanya Shree M <dhanyashree140@gmail.com>
2026-03-09 19:29:09 +0530

    Update setup_db.php

--- setup_db.php
+++ setup_db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "Dhan@122006";
+$password = "password";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
